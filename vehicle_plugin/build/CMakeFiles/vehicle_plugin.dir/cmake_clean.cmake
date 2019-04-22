d5050a0ccb4db537e7991e2b8ea755dd781ce2a7
TianqiLi7398 <tqli3@uw.edu>
2019-04-22 13:52:30 -0500

    try to add plugin in gazebo file

---
+++ vehicle_plugin/build/CMakeFiles/vehicle_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vehicle_plugin.dir/vehicle_plugin.cc.o"
+  "libvehicle_plugin.pdb"
+  "libvehicle_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vehicle_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
